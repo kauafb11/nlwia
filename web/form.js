7d2273eb6c05d75aa1bcd2ec467ce950456efc01
kauafb11 <kaua.ferreira1201@gmail.com>
2023-09-17 19:07:23 -0300

    nlwia

---
+++ web/form.js
@@ -0,0 +1,32 @@
+import { server } from "./server.js"
+
+const form = document.querySelector("#form")
+const input = document.querySelector("#url")
+const content = document.querySelector("#content")
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault()
+  content.classList.add("placeholder")
+
+  const vídeoURL = input.value
+
+  if (!vídeoURL.includes("shorts")) {
+    return (content.textContent = "Esse vídeo não parece ser um short.")
+  }
+
+  const [_, params] = vídeoURL.split("/shorts/")
+  const [vídeoID] = params.split("?si")
+
+  content.textContent = "Obtendo o texto do audio..."
+
+  const transcription = await server.get("/summary/" + vídeoID)
+
+  content.textContent = "Realizando o resumo..."
+
+  const summary = await server.post("/summary", {
+    text: transcription.data.result, 
+  })
+
+  content.textContent = summary.data.result
+  content.classList.remove("placeholder")
+})
